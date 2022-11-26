750510031cc90d93961089485e3986782d41ba67
manishabhujbal <119038857+splitsvilla@users.noreply.github.com>
2022-11-26 14:30:03 +0530

    committing to string
    Hi TL,
    regards,
    splitsvilla
    
    Co-Authored-By: manishabhujbal <118868386+manishabhujbal@users.noreply.github.com>

---
+++ project2611/src/main/java/project2611/project2611/StringProg.java
@@ -0,0 +1,14 @@
+package project2611.project2611;
+
+public class StringProg {
+	
+	public static void main(String[] args) {
+		String str="Manisha";
+		for(int i=str.length()-1;i>=0;i--)
+		{
+			char c=str.charAt(i);
+			System.out.print(c);
+		}
+	}
+
+}
